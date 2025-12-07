434de6868ec97e7f6e9be6e5363b2fba0e950ff5
DiscRoomFan36 <fletcherhogarth@gmail.com>
2025-12-07 11:26:29 +1100

    fast day_06

--- src/day_06.c
+++ src/day_06.c
@@ -5,73 +5,23 @@
 
 
 
-#define get_grid(grid, i, j)    ( (grid).items[(j) * grid_width + (i)] )
-
-
 internal Solution solve_input(String input) {
 
     Arena *allocator = Scratch_Get();
 
 
-    u32 grid_width = 0;
-    u32 grid_height;
-    String_Array grid = { .allocator = allocator };
-
 
     u64 max_line_length = 0;
 
     String_Array lines = string_split_by(input, "\n", .dont_trim_lines = true, .allocator = allocator);
-    grid_height = lines.count;
+    u32 grid_height = lines.count;
 
     for (u64 j = 0; j < lines.count; j++) {
         String line = lines.items[j];
-
         if (max_line_length < line.length)    max_line_length = line.length;
-
-        String_Array line_split = string_split_by(line, " ", .allocator = allocator);
-        for (u64 i = 0; i < line_split.count; i++) {
-            String item = line_split.items[i];
-            if (item.length == 0) {
-                Array_Remove(&line_split, i, 1);
-                i -= 1;
-            }
-        }
-
-        if (grid_width == 0) {
-            grid_width = line_split.count;
-            debug(grid_width);
-            Array_Add(&grid, grid_width*grid_height);
-        }
-
-        ASSERT(grid_width == line_split.count);
-
-        for (u64 i = 0; i < line_split.count; i++) {
-            get_grid(grid, i, j) = String_Duplicate(line_split.allocator, line_split.items[i], .null_terminate = true);
-        }
     }
 
-
-    s64 grand_total = 0;
-    for (u64 i = 0; i < grid_width; i++) {
-        char operator = get_grid(grid, i, grid_height-1).data[0];
-        bool is_plus = (operator == '+');
-
-        s64 this_total = is_plus ? 0 : 1;
-        for (u64 j = 0; j < grid_height-1; j++) {
-            String item = get_grid(grid, i, j);
-            s64 number = atoll(item.data);
-            if (is_plus) {
-                this_total += number;
-            } else {
-                this_total *= number;
-            }
-        }
-
-        grand_total += this_total;
-    }
-
-
-
+    // collect all the collums, by knowing all the correct boundrys
     Int_Array index_of_spaces = { .allocator = allocator };
     for (u64 i = 0; i < lines.items[0].length; i++) {
         char c = lines.items[0].data[i];
@@ -87,64 +37,95 @@ internal Solution solve_input(String input) {
                 i -= 1;
             }
         }
-
-        if (index_of_spaces.count == grid_width-1) break;
     }
 
 
-    // debug(index_of_spaces);
-
-    s64 correct_total = 0;
+    s64 left_to_right_total = 0;
+    s64 right_to_left_and_top_to_bottom_total = 0;
 
 
     // for all the groups
-    for (u64 i = 0; i < grid_width; i++) {
-        char operator = get_grid(grid, i, grid_height-1).data[0];
-        bool is_plus = (operator == '+');
+    for (u64 i = 0; i < index_of_spaces.count+1; i++) {
 
-        s64 this_total = is_plus ? 0 : 1;
-
-        s64 offset     = i == 0            ? 0               : index_of_spaces.items[i-1] + 1;
-        s64 next_space = i == grid_width-1 ? max_line_length : index_of_spaces.items[i];
+        s32 offset     = i == 0                     ? 0               : index_of_spaces.items[i-1] + 1;
+        s32 next_space = i == index_of_spaces.count ? max_line_length : index_of_spaces.items[i];
 
         s32 collum_width = next_space - offset;
 
-        // get one number
-        for (s32 j = 0; j < collum_width; j++) {
-            s64 this_spesific_number = 0;
 
-            for (u64 k = 0; k < grid_height-1; k++) {
-                u64 collum_index = j + offset;
-                String line = lines.items[k];
-
-                if (collum_index >= line.length) continue;
-
-                char c = line.data[collum_index];
-                if (c == ' ') continue;
-
-                this_spesific_number = this_spesific_number * 10 + (c - '0');
+        bool is_plus;
+        {
+            bool flag = false;
+            for (s32 j = offset; j < next_space; j++) {
+                char c = lines.items[lines.count-1].data[j];
+                if (c == '+' || c == '*') {
+                    is_plus = (c == '+');
+                    flag = true;
+                    break;
+                }
             }
-
-            if (is_plus) {
-                this_total += this_spesific_number;
-            } else {
-                this_total *= this_spesific_number;
-            }
+            ASSERT(flag);
         }
 
-        correct_total += this_total;
+
+        { // part 1
+            s64 this_total = is_plus ? 0 : 1;
+
+            for (u64 k = 0; k < grid_height-1; k++) {
+                String line = lines.items[k];
+                s32 number = atoi(&line.data[offset]);
+
+                if (is_plus) {
+                    this_total += number;
+                } else {
+                    this_total *= number;
+                }
+            }
+            left_to_right_total += this_total;
+        }
+
+
+        { // part 2
+            // get one number
+            s64 this_total = is_plus ? 0 : 1;
+
+            for (s32 j = 0; j < collum_width; j++) {
+                s64 this_specific_number = 0;
+
+                for (u64 k = 0; k < grid_height-1; k++) {
+                    u64 collum_index = j + offset;
+                    String line = lines.items[k];
+
+                    if (collum_index >= line.length) continue;
+
+                    char c = line.data[collum_index];
+                    if (c == ' ') continue;
+
+                    this_specific_number = this_specific_number * 10 + (c - '0');
+                }
+
+                if (is_plus) {
+                    this_total += this_specific_number;
+                } else {
+                    this_total *= this_specific_number;
+                }
+            }
+
+            right_to_left_and_top_to_bottom_total += this_total;
+        }
+
     }
 
 
     Solution solution = {
-        .part_1 = grand_total,
-        .part_2 = correct_total,
+        .part_1 = left_to_right_total,
+        .part_2 = right_to_left_and_top_to_bottom_total,
     };
     return solution;
 }
 
 
-#define DAY "XX"
+#define DAY "06"
 
 int main(void) {
     printf("                             Solving Day %s!\n", DAY);
@@ -154,7 +135,7 @@ int main(void) {
 
     Do_Input();
 
-    // Perf_Input(Get_Input(), 10);
+    // Perf_Input(Get_Input(), 1000);
 
     printf("======================================================================================\n");
     Scratch_Free();
